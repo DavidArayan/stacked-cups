4aba04fd875ff6afd2c257adf5b130f5d1a96024
David Arayan <david.arayan12@gmail.com>
2019-04-28 18:38:11 +1000

    - Added initial implementation of the FillCups in UI mode

--- stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/views/PageFillCup.java
+++ stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/views/PageFillCup.java
@@ -1,6 +1,9 @@
 package com.arayan.stackedcups.controller.views;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -9,7 +12,19 @@ import com.arayan.stackedcups.model.interfaces.CupLUT;
 
 public final class PageFillCup extends BaseView {
 
+    private final TextView currentLiquid;
+
     public PageFillCup(@NonNull final CupLUT lut, @NonNull final Activity mainActivity) {
         super(lut, R.layout.page_fillcup, mainActivity);
+
+        currentLiquid = getView().findViewById(R.id.fillcups_current);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void show(@NonNull final ViewGroup parent) {
+        super.show(parent);
+
+        currentLiquid.setText("Total Liquid is " + getLut().getTotalVolume() + "ml");
     }
 }
