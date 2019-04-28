139646465a7edb6e82b716b77ce1db09012cabd0
David Arayan <david.arayan12@gmail.com>
2019-04-28 14:15:20 +1000

    - Added skeleton code for view navigation - Added navigation error handlers via Toast messages

--- stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/MainActivity.java
+++ stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/MainActivity.java
@@ -9,38 +9,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    private View holderView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_fill_cups:
-                    mTextMessage.setText(R.string.title_fill_cups);
-                    return true;
-                case R.id.navigation_query:
-                    mTextMessage.setText(R.string.title_query_cups);
-                    return true;
-                case R.id.navigation_about:
-                    mTextMessage.setText(R.string.title_about);
-                    return true;
-            }
-            return false;
-        }
-    };
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navigation_fill_cups:
+                        showCupsView();
+                        return true;
+                    case R.id.navigation_query:
+                        showQueryView();
+                        return true;
+                    case R.id.navigation_about:
+                        showAboutView();
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+        holderView = findViewById(R.id.holder_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    private void showCupsView() {
+        removeAllChildViews();
+    }
+
+    private void showQueryView() {
+        removeAllChildViews();
+    }
+
+    private void showAboutView() {
+        removeAllChildViews();
+    }
+
+    private void removeAllChildViews() {
+        if (holderView != null && holderView instanceof ViewGroup) {
+            ((ViewGroup)holderView).removeAllViews();
+        }
+        else {
+            final Toast toast = Toast.makeText(this, getString(R.string.navigation_failure), Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
 }
