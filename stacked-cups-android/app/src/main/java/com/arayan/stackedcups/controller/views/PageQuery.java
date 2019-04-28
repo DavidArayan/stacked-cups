475834b6a56de4b2bacf403ed44a8356d4b8416f
David Arayan <david.arayan12@gmail.com>
2019-04-28 19:24:28 +1000

    - Added full implementation of the PageQuery

--- stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/views/PageQuery.java
+++ stacked-cups-android/app/src/main/java/com/arayan/stackedcups/controller/views/PageQuery.java
@@ -1,6 +1,5 @@
 package com.arayan.stackedcups.controller.views;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -10,6 +9,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 
 import com.arayan.stackedcups.R;
+import com.arayan.stackedcups.model.interfaces.Cup;
 import com.arayan.stackedcups.model.interfaces.CupLUT;
 
 public final class PageQuery extends BaseView {
@@ -28,7 +28,22 @@ public final class PageQuery extends BaseView {
 
         // on button click, we check the volume that we need to add and add it in
         showLiquidButton.setOnClickListener(view -> {
+            try {
+                final int cupRowIndex = Integer.valueOf(liquidRowAddView.getText().toString());
+                final int cupColIndex = Integer.valueOf(liquidColAddView.getText().toString());
 
+                if (cupColIndex > cupRowIndex || cupRowIndex < 0 || cupColIndex < 0) {
+                    Toast.makeText(getView().getContext(), "provided input was not within valid input range", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    final Cup cup = getLut().getCup(cupRowIndex, cupColIndex);
+
+                    updateVolumeDisplay("liquid at cup(" + cupRowIndex + "," + cupColIndex + ") is " + cup.getCurrentVolume() + "ml");
+                }
+            }
+            catch (final NumberFormatException e) {
+                Toast.makeText(getView().getContext(), "Input was invalid", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
@@ -36,7 +51,7 @@ public final class PageQuery extends BaseView {
     public void show(@NonNull final ViewGroup parent) {
         super.show(parent);
 
-        updateVolumeDisplay("volume at cup(0,0) is " + getLut().getRootCup().getCurrentVolume() + "ml");
+        updateVolumeDisplay("liquid at cup(0,0) is " + getLut().getRootCup().getCurrentVolume() + "ml");
     }
 
     private void updateVolumeDisplay(final String text) {
